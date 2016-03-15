f2979b0f64bcf82314f666bad7ca3d2ca6486cca
Jonathan Guinto <xianlee.ivan.guinto@gmail.com>
2016-03-15 16:18:13 +0800

    Revert "add CardController"
    This reverts commit 55d444fa8f9da3102ddabb164f310c296c21038d.

--- DominionCardTracker.DataLayer/Repository/CardModifierRepository.cs
+++ DominionCardTracker.DataLayer/Repository/CardModifierRepository.cs
@@ -3,7 +3,6 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using Dapper;
-using DominionCardTracker.Models.Tables;
 using DominionCardTracker.Models.Views;
 
 namespace DominionCardTracker.DataLayer.Repository
@@ -19,31 +18,6 @@ namespace DominionCardTracker.DataLayer.Repository
                 return
                     connection.Query<CardModifierView>("CardModifierSelectByCardId", p,
                         commandType: CommandType.StoredProcedure).ToList();
-            }
-        }
-
-        public void Insert(CardModifier cardModifier)
-        {
-            using (var connection = new SqlConnection(ConfigurationSettings.GetConnectionString()))
-            {
-                var p = new DynamicParameters();
-                p.Add("@CardId", cardModifier.CardId);
-                p.Add("@ModifierTypeId", cardModifier.ModifierTypeId);
-                p.Add("@ModifierValue", cardModifier.ModifierValue);
-                p.Add("@InstructionText", cardModifier.InstructionText);
-
-                connection.Execute("CardModifierInsert", p, commandType: CommandType.StoredProcedure);
-            }
-        }
-
-        public void Delete(int cardModifierId)
-        {
-            using (var connection = new SqlConnection(ConfigurationSettings.GetConnectionString()))
-            {
-                var p = new DynamicParameters();
-                p.Add("@CardModifierId", cardModifierId);
-
-                connection.Execute("CardModifierDelete", p, commandType: CommandType.StoredProcedure);
             }
         }
     }
--- DominionCardTracker.Web/Controllers/CardsController.cs
+++ DominionCardTracker.Web/Controllers/CardsController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Net;
 using System.Web.Mvc;
 using DominionCardTracker.DataLayer.Repository;
 using DominionCardTracker.Models.Tables;
@@ -45,41 +44,7 @@ namespace DominionCardTracker.Web.Controllers
 
         public ActionResult Edit(int id)
         {
-            var cardSetRepo = new CardSetRepository();
-            var cardRepo = new CardRepository();
-            var modifierTypeRepo = new ModifierTypeRepository();
-
-            var model = new CardEditModel(cardRepo.SelectView(id), cardSetRepo.SelectAll(), modifierTypeRepo.SelectAll());
-
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult Edit(Card card)
-        {
-            var cardRepo = new CardRepository();
-            cardRepo.Update(card);
-
-            return RedirectToAction("Index", "Cards");
-        }
-
-        [HttpPost]
-        public ActionResult Removemodifier(int id)
-        {
-            var cardModifierRepo = new CardModifierRepository();
-            cardModifierRepo.Delete(id);
-
-            return new HttpStatusCodeResult(HttpStatusCode.OK);
-        }
-
-        [HttpPost]
-        public JsonResult AddModifier(CardModifier model)
-        {
-            var cardModifierRepo = new CardModifierRepository();
-            var cardRepo = new CardRepository();
-
-            cardModifierRepo.Insert(model);
-            return Json(cardRepo.SelectView(model.CardId), JsonRequestBehavior.AllowGet);
+            return View();
         }
     }
 }
